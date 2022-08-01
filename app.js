7da6148418da98f1237e5ea4cf148db5d65b9909
Abdullah Soomro <abdullahsoomor.official@gmail.com>
2022-08-01 18:58:37 +0500

    Design is improved now

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-const item0 = document.querySelector('.item-0');
-const item1 = document.querySelector('.item-1');
-const item2 = document.querySelector('.item-2');
-const item3 = document.querySelector('.item-3');
-const item4 = document.querySelector('.item-4');
-const item5 = document.querySelector('.item-5');
-const item6 = document.querySelector('.item-6');
-const item7 = document.querySelector('.item-7');
-const item8 = document.querySelector('.item-8');
-const item9 = document.querySelector('.item-9');
-const item10 = document.querySelector('.item-10');
-const item11 = document.querySelector('.item-11');
-const item12 = document.querySelector('.item-12');
-const item13 = document.querySelector('.item-13');
-const item14 = document.querySelector('.item-14');
-const item15 = document.querySelector('.item-15');
-const item16 = document.querySelector('.item-16');
+const zero = document.querySelector('.item-14');
+const one = document.querySelector('.item-11');
+const two = document.querySelector('.item-10');
+const three = document.querySelector('.item-9');
+const four = document.querySelector('.item-5');
+const five = document.querySelector('.item-6');
+const six = document.querySelector('.item-7');
+const seven = document.querySelector('.item-1');
+const eight = document.querySelector('.item-2');
+const nine = document.querySelector('.item-3');
+const period = document.querySelector('.item-13');
+const divide = document.querySelector('.item-4');
+const mutltiply = document.querySelector('.item-8');
+const minus = document.querySelector('.item-12');
+const plus = document.querySelector('.item-16');
+const equalTo = document.querySelector('.item-15');
+const screen = document.querySelector('.item-0');
+const clearScreen = document.querySelector('.item-17');
+
+
+const buttons = [zero, one, two, three, four, five, six, seven, eight, nine, period, divide, mutltiply, minus, plus];
+
+buttons.forEach(function(element) {
+    element.addEventListener('click', function() {
+        const value = this.innerText;
+        screen.innerText += value;
+    })
+})
